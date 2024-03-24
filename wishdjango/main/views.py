a14c94b2a8565d9d46fa9fa27a2f098fda846ce1
ym1021 <ym1021@cu.ac.kr>
2024-03-24 23:31:56 +0900

    first commit

---
+++ wishdjango/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+class ExampleView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello from Django!"})
